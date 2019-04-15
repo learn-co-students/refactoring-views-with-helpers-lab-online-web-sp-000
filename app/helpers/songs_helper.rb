f7b6ef1e85ce211e922446e4e2cb2d2df56273f3
jonmackeyparty <jon@jonmackey.party>
2019-04-15 12:43:10 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
+  def display_artist(song)
+    if !song.artist_name
+      link_to "Add Artist", edit_song_path(song.id)
+    else
+      link_to "#{song.artist_name}",  artist_path(song.artist)
+    end
+  end
 
 end
