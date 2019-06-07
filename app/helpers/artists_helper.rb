4af28c1836a0f90749c61444678b0c76f7f314d8
Diana Badas <diana.badas@gmail.com>
2019-06-07 10:58:03 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,6 +5,5 @@ module ArtistsHelper
     else
       link_to "Add Artist", edit_song_path(song)
     end
-
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    artist.name if artist
+    artist.name
   end
 
   def artist_name=(name)
