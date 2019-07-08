8536546bc29dbdc106cab23612c3aa89925736df
Rachel <williamsrachelada@gmail.com>
2019-07-08 14:02:54 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
 
+    def display_artist(song)
+      artist = song.artist
+      if artist
+        link_to artist.name, artist_path(artist)
+      else
+        link_to "Add Artist", edit_song_path(song)
+      end
+    end
+
 end
