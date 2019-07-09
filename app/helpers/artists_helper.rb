89d4f449192718b5d046acd061122aaf3eab9f81
Daniel Dawson <dannyldawson@gmail.com>
2019-07-09 14:04:35 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if artist = song.artist
+      link_to artist.name, artist
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
