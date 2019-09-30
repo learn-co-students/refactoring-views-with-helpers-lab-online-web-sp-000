6c33a2cc636bb3da3520c8e99458ee11cfba335f
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-30 00:20:56 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
 
   def display_artist(song)
-    if artist_name
-      link_to song.artist_name, artist_path(song.artist)
-    else
+    if song.artist.nil?
       link_to "Edit Song", edit_song_path(song)
+    else
+      link_to song.artist_name, artist_path(song.artist)
   end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
 
-  def display_artist(song)
-    if artist_name
-      link_to artist.name, artist_path(artist.name)
-    else
-      link_to
-  end
 end
