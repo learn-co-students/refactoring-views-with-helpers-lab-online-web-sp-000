e8999d7f2efc687e9be56269412c62d5c85e2ef0
BlythePollard <github email address>
2020-02-03 17:47:59 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist
+            link_to song.artist.name, artist_path(song.artist)
+        else 
+            link_to 'Add Artist', edit_song_path(song)
+        end
+        #if artist is associated with song, link to artist show page
+        #else, link to edit page with text "Add artist"
+    end
 end
