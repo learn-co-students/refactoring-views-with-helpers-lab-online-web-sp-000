09d1a42d733031b3baaa581a8ec1d29bd507672e
rohanphillips <rohan@solutions-insurance.com>
2020-02-17 12:37:20 -0600

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,21 +1,12 @@
 module SongsHelper
-  def artist_name(song)
-    if song.artist
-      song.artist.name
-    else
-      "Add Artist"
-    end
-  end
+  # def artist_name(song)
+  #   if song.artist
+  #     song.artist.name
+  #   end
+  # end
 
   def artist_id(song)
     @song.artist.id
   end
 
-  def display_artist(song)
-    if song.artist
-      artists_path + "/" + song.artist.id.to_s
-    else
-      edit_song_path(song)
-    end
-  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,26 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  
+  def artist_name
+    if artist
+      artist.name
+    end
+  end
+
+  def artist_name=(name)
+    if artist
+      artist.name = name
+    else
+      if Artist.exists?(name: name)
+        self.artist = Artist.where(name: name).first
+        self.save
+      else
+        a = Artist.new
+        a.name = name
+        a.save
+        self.artist = a
+        self.save
+      end
+    end
+  end
 end
