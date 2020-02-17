aa56122a2c91e33512ca2f68b8c5da9f5add6ee0
rohanphillips <rohan@solutions-insurance.com>
2020-02-17 08:46:29 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_name(song)
+    song.artist.name
   end
 
-  def artist_name=(name)
+  def artist_id(song)
+    @song.artist.id
   end
 end
