5f0e29f8074dfc5b2b044c3be23c3517e9d11163
Alvin Coulson <aaacoulson@gmail.com>
2020-06-08 22:24:28 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist
+            link_to song.artist.name, song.artist
+        else
+            link_to "Add Artist to song", edit_song_path(song)
+        end
+    end
 end
