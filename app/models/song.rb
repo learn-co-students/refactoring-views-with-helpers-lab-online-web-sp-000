541a9214dc5ec090d5a72c47ba8bef7eb8c3408a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-29 23:50:28 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist ? self.artist.name : nil
+    # self.artist ? self.artist.name : nil
+    artist.name if artist
   end
 
   def artist_name=(name)
