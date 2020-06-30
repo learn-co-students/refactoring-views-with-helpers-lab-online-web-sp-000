f862e101208c49865b394383d1efe97dfe75366e
CoderJay06 <jaycru06@yahoo.com>
2020-06-30 16:17:57 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,3 +1,13 @@
 module ArtistsHelper
-   
+   def display_artist(song)
+      if song.artist.present?
+         # if an artist is associated with the song
+         # return link to artists's show page 
+         link_to song.artist_name, artist_path(song.artist)
+      else  
+         # if an artist is not associated with song 
+         # return a link to the song's edit page "Add Artist"
+         link_to "Add Artist", edit_song_path(song)
+      end 
+   end 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist
+    self.artist.name
   end
 
   def artist_name=(name)
-    self.artist = name 
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
