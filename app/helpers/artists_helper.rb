88c55443fba248ab13e71da533eff8c5871ba896
ksummerill <kaitlynsummerill@gmail.com>
2020-07-17 07:30:37 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+  # The method's return value should take into consideration
+  # whether an artist is already associated with a song
+  def display_artist(artist_name)
+    if artist_name
+      link_to artist_path
+    else artist_name.empty?
+      link_to "Add Artist", song_path(edit)
+    end
+  end
 end
