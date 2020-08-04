c7615b77f7aef7fdea76f201a11658995711e4e8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 23:25:16 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,8 @@
 module ArtistsHelper
 
+#responds to display artist with one argument
+# displays a link to edit song if artist empty
+# displays a link to artist page if not empty 
   def display_artist(song)
     if song.artist
       link_to song.artist_name, artist_path(song.artist)
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,6 @@
 module SongsHelper
 
+#can get artist name, set artist via name, find artist if exists 
   def display_artist(song)
     if song.artist
       link_to song.artist.name, artist_path(song.artist)
@@ -11,4 +12,4 @@ end
 
 #to be called in our views
 # methods return value should see if artist is already associated with a song
-# if not associated, return a link to song edit page to add artist 
+# if not associated, return a link to song edit page to add artist
