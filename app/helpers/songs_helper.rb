27109089ada803af41340984172baa2f241e77c9
Elliot Blanchard <git@elliotblanchard.com>
2020-08-04 22:05:09 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,14 @@
 module SongsHelper
 
+    def display_artist(song)
+        if song.artist != nil 
+            if song.artist.name.length > 0
+                link_to song.artist_name, artist_path(song.artist)
+            else
+                link_to "Add Artist", edit_song_path(song)
+            end
+        else
+            link_to "Add Artist", edit_song_path(song)  
+        end
+    end
 end
