d43a2d5c71f6ad8910dbc29112a419a8ddb068fe
Sergio Marelli <sergio.mrll@gmail.com>
2020-08-29 09:27:48 +0100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+  # accepts_nested_attributes_for :artist_name
+
   def artist_name
+  	artist.name
   end
 
   def artist_name=(name)
+  	artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 end
