dcce71ac9f8f394878641b058474704cd0e8aadc
The-Gentleman <zraja4402@gmail.com>
2020-09-06 20:09:59 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,9 @@
 module ArtistsHelper
     def display_artist(song)
-        if song.artist.nil?
-            link_to song.artist, edit_song_path(song)
+        if song.artist
+            link_to song.artist.name, song.artist
+        else 
+            link_to "Add Artist", edit_song_path(song)
         end 
     end 
 end
