f6171a739166e8ec0ac8fd10876f3199648f0acf
Yumacorn <ReachRyanMui@gmail.com>
2020-09-27 11:26:06 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def display_artist
+        if artist
+            <h3><%= link_to @song.artist_name, artist_path(@song.artist_id) %></h3>
+        else
+            <h3><%= link_to "Add Artist", song_path(@song)/edit %></h3>
+        end
+    end
 end
