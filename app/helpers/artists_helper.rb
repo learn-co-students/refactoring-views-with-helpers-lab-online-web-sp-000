3d8b3c1c885b6c27cb5d88cb06579bf7c5d6d071
roadpilot <mdacht777@gmail.com>
2020-10-04 18:11:21 -0600

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist (song)
+            link_to song.artist.name, artist_path(song.artist)
+        else 
+            link_to "Add Artist", "#{song_path(song)}/edit"
+        end 
+    end
+
+
 end
