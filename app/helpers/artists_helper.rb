4521d69270ddf26d8cac1a39a06196d98f7a4161
khoinguyenkc <khoinguyenkc@gmail.com>
2020-11-16 19:26:27 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,16 @@
 module ArtistsHelper
+    def display_artist(song)
+        binding.pry
+        #parameter = a song instance
+        if song.artist #artist exist, not nil
+             link_to song.artist.name, artist_path(song.artist) 
+        else 
+            link_to "Add Artist", edit_song_path(song)
+        end
+
+    end
+
 end
+
+
+
