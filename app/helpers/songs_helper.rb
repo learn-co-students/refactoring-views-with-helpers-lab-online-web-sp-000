a3a917167667cbf15a64a8432017701881626851
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-29 17:00:35 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
-
+  def display_artist(song)
+         # binding.pry
+         if song.artist
+             # binding.pry
+             link_to song.artist.name, artist_path(song.artist)
+         else
+             # binding.pry
+             link_to "Add Artist", controller: "songs", action: "edit", id: song.id
+         end
+     end
 end
