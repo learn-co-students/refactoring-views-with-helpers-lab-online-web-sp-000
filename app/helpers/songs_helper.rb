d487c67c5188157eb7d4abfc22e3c01c7d5658db
A.J. Romaniello <ajiellodev@gmail.com>
2021-03-16 13:52:14 -0600

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+  def display_artist(song)
+    if song.artist
+      link_to song.artist_name, song.artist
+    else
+      link_to 'Add Artist', edit_artist_path(song.artist)
+    end
+  end
 end
