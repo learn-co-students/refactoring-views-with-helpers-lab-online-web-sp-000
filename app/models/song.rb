335b2bf748e3c3030771a56d11929ec4c2f7f149
a-laz <mortaleusmarketing@gmail.com>
2021-03-17 13:51:37 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_attributes=(artist_attributes)
+    self.artist = Artist.where(name: artist_attributes[:name]).first_or_create
   end
 
-  def artist_name=(name)
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.where(name: artist_name).first_or_create
   end
 end
