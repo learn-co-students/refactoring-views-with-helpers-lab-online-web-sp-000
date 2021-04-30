0b9d4b10402bed24244722402cca40289770cbe4
Amanda King <amapickell@gmail.com>
2021-04-29 20:30:00 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.where(name: name).first_or_create do |a|
+      a.name = name
+    end
   end
 end
