d826611935be1428163ac0971114414274ab0a7c
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-05-06 15:51:04 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,15 +1,3 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-
-  private
-  
-  def artist_name
-    @song = Song.find(params[:id])
-    @song.artist.name
-  end
-
-  def artist_name=(name)
-    @song = Song.find(params[:id])
-    @song.artist.name = Artist.find_or_create_by(name)
-  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
-  def artist_name=(name)
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(artist_name)
+    self.artist.name
   end
 end
