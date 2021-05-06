267cd5cbb29e4e22be88c67f614cbebfc75bc58f
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-05-06 13:48:48 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+    def display_artist(artist)
+        @song = Song.find(params[:id])
+        if artist == @song.artist
+            link_to artist_path(artist)
+        else
+            link_to edit_song_path(@song)
+        end
+    end
 end
